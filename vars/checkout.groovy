bf3d2b1eef6ad8982d464a467470bf4cf0226b1f
Ramagopalan <Ramji_Ramagopalan@Dell.com>
2020-10-05 14:28:52 +0530

    adding dir

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -8,12 +8,22 @@ node('master') {
 
    stage('Checkout') {
    	echo "Checkout source code"
-   	echo "${BUILD_URL}"
-   	echo  "job url is $buildurl"
-   	//sh '''curl -i -XPOST http://3.134.86.192:8086/query --data-urlencode "q=CREATE DATABASE mydb"
-//curl -i -XPOST \'http://3.134.86.192:8086/write?db=mydb\' --data-binary \'stagecheck,STAGE=CHECKOUT,buildurl=${BUILD_URL},buildid=$BUILD_ID,jobname=${JOB_NAME},Presence=Yes value=1\''''
-   
-   sh 'curl -i -XPOST \'http://3.134.86.192:8086/write?db=mydb\' --data-binary "jenkins_stage_data,buildurl=${buildurl},Stage=CHECKOUT value=1"'
+   	sh '''build_url=${BUILD_URL}
+jobname=${JOB_NAME}
+
+echo "BUILD_URL is $build_url"
+
+var1="buildurl="
+
+echo "var1 is $var1"
+
+buildurl="$var1$build_url"
+echo " build url is $buildurl"'''
+
+
+
+
+sh 'curl -i -XPOST \'http://3.134.86.192:8086/write?db=mydb\' --data-binary "jenkins_stage_data,buildurl=$build_url,jobname=$jobname,Stage=Build value=1"'
 
 }
 
