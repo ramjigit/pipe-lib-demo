94afc5f590d785c6bcc1cfc6e3dbaf12605e2d0f
Ramagopalan <Ramji_Ramagopalan@Dell.com>
2020-10-05 16:25:02 +0530

    adding dir

--- vars/ifdbpush.groovy
+++ vars/ifdbpush.groovy
@@ -13,6 +13,39 @@ node('master') {
 	}
    
    stage('UNIT TEST') {
+   	// Get InfluxDB plugin descriptor (version < 2.0)
+def influxdb = Jenkins.instance.getDescriptorByType(jenkinsci.plugins.influxdb.DescriptorImpl)
+
+// version >= 2.0
+def influxdb = Jenkins.instance.getDescriptorByType(jenkinsci.plugins.influxdb.InfluxDbStep.DescriptorImpl)
+
+// Create target
+def target = new jenkinsci.plugins.influxdb.models.Target()
+
+// Set target details
+
+// Mandatory fields
+target.description = 'my-new-target'
+target.url = 'http://3.134.86.192:8086'
+target.username = 'admin'
+
+// version < 2.0
+target.password = 'Rahul@18'
+
+// version >= 2.0
+target.password = hudson.util.Secret.fromString('Rahul@18')
+
+target.database = 'mydb'
+
+// Optional fields
+target.retentionPolicy = '1d'                    // default = 'autogen'
+target.jobScheduledTimeAsPointsTimestamp = true  // default = false
+target.exposeExceptions = true                   // default = true
+target.usingJenkinsProxy = true                  // default = false
+
+// Add a target by using the created target object
+influxdb.addTarget(target)
+influxdb.save()
    	echo "Unit testing the code"
    	def sFields = [:]
      sFields['field_k'] = 'UNITTEST'
