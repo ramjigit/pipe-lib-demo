f80b243b27ca59796d0983e412a9ed1f5986a7f8
Ramagopalan <Ramji_Ramagopalan@Dell.com>
2020-10-20 23:07:02 +0530

    add

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -13,34 +13,18 @@ node('master') {
    	println param1
    	ck = param1
 sh """build_url=${BUILD_URL}
+      jobname=${JOB_NAME}
 
-echo "ck is $ck"
-if [ $ck == 1 ]
-then
-jobname=${JOB_NAME}
-buildid=${BUILD_ID}
+    echo "ck is $ck"
+    if [ $ck == 1 ]
+     then
+         echo "BUILD_URL is $build_url"
 
-echo "BUILD_URL is $build_url"
-
-
-
-buildurl="$var1$build_url"
-echo " build url is $buildurl"
+          echo " build url is $buildurl"
 curl -i -XPOST \'http://3.134.86.192:8086/write?db=mydb\' --data-binary "j_s_d,buildurl=$build_url,jobname=$jobname Checkout=$ck"  
 else
-jobname=${JOB_NAME}
-buildid=${BUILD_ID}
 
-echo "BUILD_URL is $build_url"
-
-var1="buildurl="
-
-echo "var1 is $var1"
-
-buildurl="$var1$build_url"
-echo " build url is $buildurl"
-echo " Ramji Ramji  Ramji"
-curl -i -XPOST \'http://3.134.86.192:8086/write?db=mydb\' --data-binary "j_s_d,buildurl=$build_url,jobname=$jobname Checkout=ck"
+curl -i -XPOST \'http://3.134.86.192:8086/write?db=mydb\' --data-binary "j_s_d,buildurl=$build_url,jobname=$jobname Checkout=0"
 fi"""
 
 }
