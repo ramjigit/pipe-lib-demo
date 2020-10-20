048e32c93e8f67e191023816ca64c4d5035d2eb0
Ramagopalan <Ramji_Ramagopalan@Dell.com>
2020-10-20 23:10:43 +0530

    add

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -3,6 +3,7 @@ def call(int param1){
 node('master') {
    environment {
         buildurl = "${BUILD_URL}"
+        jobname="${JOB_NAME}"
         ck=param1
         
         
@@ -12,18 +13,18 @@ node('master') {
    	echo "Checkout source code"
    	println param1
    	ck = param1
-sh """build_url=${BUILD_URL}
-      jobname=${JOB_NAME}
+sh """
 
     echo "ck is $ck"
     if [ $ck == 1 ]
      then
          echo "BUILD_URL is $build_url"
+         echo  " jobname  is $jobname"
 
-curl -i -XPOST \'http://3.134.86.192:8086/write?db=mydb\' --data-binary "j_s_d,buildurl=$build_url,jobname=$jobname Checkout=$ck"  
+curl -i -XPOST \'http://3.134.86.192:8086/write?db=mydb\' --data-binary "j_s_d,buildurl=$buildurl,jobname=$jobname Checkout=$ck"  
 else
 
-curl -i -XPOST \'http://3.134.86.192:8086/write?db=mydb\' --data-binary "j_s_d,buildurl=$build_url,jobname=$jobname Checkout=0"
+curl -i -XPOST \'http://3.134.86.192:8086/write?db=mydb\' --data-binary "j_s_d,buildurl=$buildurl,jobname=$jobname Checkout=0"
 fi"""
 
 }
