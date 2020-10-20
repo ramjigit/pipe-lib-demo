ae8a22f259dc63924f30494fd8fa9520c731f9d6
Ramagopalan <Ramji_Ramagopalan@Dell.com>
2020-10-20 21:21:19 +0530

    add

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -1,5 +1,5 @@
 #!/usr/bin/env groovy
-def call(nodename){
+def call(nodename,checkout){
 node(nodename) {
    environment {
         buildurl = "${BUILD_URL}"
@@ -9,6 +9,8 @@ node(nodename) {
    stage('Checkout') {
    	echo "Checkout source code"
 sh '''build_url=${BUILD_URL}
+if [ $checkout == 1 ]
+then
 jobname=${JOB_NAME}
 buildid=${BUILD_ID}
 
@@ -20,7 +22,21 @@ echo "var1 is $var1"
 
 buildurl="$var1$build_url"
 echo " build url is $buildurl"
-curl -i -XPOST \'http://3.134.86.192:8086/write?db=mydb\' --data-binary "jenkins_stages_data,buildurl=$build_url,jobname=$jobname,Stage=Checkout  Checkout=1"'''
+curl -i -XPOST \'http://3.134.86.192:8086/write?db=mydb\' --data-binary "j_s_d,buildurl=$build_url,jobname=$jobname Checkout=$checkout"  
+else
+jobname=${JOB_NAME}
+buildid=${BUILD_ID}
+
+echo "BUILD_URL is $build_url"
+
+var1="buildurl="
+
+echo "var1 is $var1"
+
+buildurl="$var1$build_url"
+echo " build url is $buildurl"
+curl -i -XPOST \'http://3.134.86.192:8086/write?db=mydb\' --data-binary "j_s_d,buildurl=$build_url,jobname=$jobname Checkout=$checkout"
+fi'''
 
 }
 
--- vars/compile.groovy
+++ vars/compile.groovy
@@ -1,5 +1,5 @@
 #!/usr/bin/env groovy
-def call(){
+def call(compile){
 node('master') {
    environment {
         BUILD_URL = "${BUILD_URL}"
@@ -9,13 +9,26 @@ node('master') {
    stage('Compile') {
    	echo "Compile"
    	sh '''build_url=${BUILD_URL}
+ if [ $checkout == 1 ]
+then
 jobname=${JOB_NAME}
 echo "BUILD_URL is $build_url"
 var1="buildurl="
 echo "var1 is $var1"
 buildurl="$var1$build_url"
 echo " build url is $buildurl"
-curl -i -XPOST \'http://3.134.86.192:8086/write?db=mydb\' --data-binary "jenkins_stages_data,buildurl=$build_url,jobname=$jobname,Stage=Compile Compile=1"'''
+curl -i -XPOST \'http://3.134.86.192:8086/write?db=mydb\' --data-binary "jenkins_stages_data,buildurl=$build_url,jobname=$jobname,Stage=Compile Compile=$compile 
+else
+jobname=${JOB_NAME}
+echo "BUILD_URL is $build_url"
+var1="buildurl="
+echo "var1 is $var1"
+buildurl="$var1$build_url"
+echo " build url is $buildurl"
+curl -i -XPOST \'http://3.134.86.192:8086/write?db=mydb\' --data-binary "jenkins_stages_data,buildurl=$build_url,jobname=$jobname,Stage=Compile Compile=$compile 
+fi
+
+"'''
 
 
 
