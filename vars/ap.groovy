ec41a4a82b4f9c3f3712046156b0049e944642f9
Ramagopalan <Ramji_Ramagopalan@Dell.com>
2020-10-21 09:22:57 +0530

    adding parameter

--- vars/ap.groovy
+++ vars/ap.groovy
@@ -4,7 +4,7 @@ node('master') {
    environment {
         buildurl = "${BUILD_URL}"
         jobname="${JOB_NAME}"
-        ArtifactoryPush=param1
+        ap1=param1
         
         
     }
@@ -20,7 +20,7 @@ node('master') {
         echo "var1 is $var1"
         buildurl="$var1$build_url"
         echo " build url is $buildurl"
-        curl -i -XPOST \'http://3.134.86.192:8086/write?db=mydb\' --data-binary "j_s,buildurl=$build_url,jobname=$jobname ArtifactoryPush=$ArtifactoryPush"'''
+        curl -i -XPOST \'http://3.134.86.192:8086/write?db=mydb\' --data-binary "j_s,buildurl=$build_url,jobname=$jobname ArtifactoryPush=$ap1"'''
 
 }
 
--- vars/vs.groovy
+++ vars/vs.groovy
@@ -4,7 +4,7 @@ node('master') {
    environment {
         buildurl = "${BUILD_URL}"
         jobname="${JOB_NAME}"
-        Vulnerabilityscan=param1
+        vscan=param1
         
         
     }
@@ -20,7 +20,7 @@ node('master') {
         echo "var1 is $var1"
         buildurl="$var1$build_url"
         echo " build url is $buildurl"
-        curl -i -XPOST \'http://3.134.86.192:8086/write?db=mydb\' --data-binary "j_s,buildurl=$build_url,jobname=$jobname Vulnerabilityscan=$Vulnerabilityscan"'''
+        curl -i -XPOST \'http://3.134.86.192:8086/write?db=mydb\' --data-binary "j_s,buildurl=$build_url,jobname=$jobname Vulnerabilityscan=$vscan"'''
 
 }
 
