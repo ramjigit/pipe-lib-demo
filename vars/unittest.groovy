bcf13dc1029213c571c36d29fa5cb0c22848feb3
Ramagopalan <Ramji_Ramagopalan@Dell.com>
2020-10-21 09:09:39 +0530

    adding parameter

--- vars/unittest.groovy
+++ vars/unittest.groovy
@@ -4,24 +4,25 @@ node('master') {
    environment {
         buildurl = "${BUILD_URL}"
         jobname="${JOB_NAME}"
-        ut=param1
+        Unittest=param1
         
         
     }
 
    stage('Unittest') {
-   	echo "unit test"
+   	echo "Unittest source code"
    	
    	sh '''build_url=${BUILD_URL}
         jobname=${JOB_NAME}
         echo "BUILD_URL is $build_url"
-        echo "unittest value is $unittest"
+        echo "Unittest value is $Unittest"
         var1="buildurl="
         echo "var1 is $var1"
         buildurl="$var1$build_url"
         echo " build url is $buildurl"
-        curl -i -XPOST \'http://3.134.86.192:8086/write?db=mydb\' --data-binary "j_s,buildurl=$build_url,jobname=$jobname unittest=$ut"'''
+        curl -i -XPOST \'http://3.134.86.192:8086/write?db=mydb\' --data-binary "j_s,buildurl=$build_url,jobname=$jobname Unittest=$Unittest"'''
 
 }
+
 }
 }
