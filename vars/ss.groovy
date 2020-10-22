7df01fe07b59bf6d40d8653f256241ebf17df133
Ramagopalan <Ramji_Ramagopalan@Dell.com>
2020-10-22 18:10:18 +0530

    add

--- vars/ss.groovy
+++ vars/ss.groovy
@@ -4,6 +4,7 @@ node('master') {
    environment {
         buildurl = "${BUILD_URL}"
         jobname="${JOB_NAME}"
+        bd="${BUILD_ID}"
         BUILD_STRING = "checkout"
         
         
@@ -15,7 +16,7 @@ node('master') {
   def job = Jenkins.instance.items.find { it.name == "${JOB_NAME}" }
    for (build in job.builds) {
       def log = build.log
-      if (log.contains('checkout')) {
+      if (log.contains('checkout' ))  && (${build.id} == ${bd}) {
        println "${job.name}: ${build.id}"
                        }
                     }
