8d9d8eac8cbb71d48da852c77edcb6bf1a150320
Ramagopalan <Ramji_Ramagopalan@Dell.com>
2020-10-22 18:02:33 +0530

    add ss

--- vars/ss.groovy
+++ vars/ss.groovy
@@ -13,12 +13,13 @@ node('master') {
    stage('Push 0 to stages not performed') {
 
   def job = Jenkins.instance.items.find { it.name == "${JOB_NAME}" }
-for (build in job.builds) {
-  def log = build.log
-  if (log.contains('checkout()')) {
-    println "${job.name}: ${build.id}"
-  }
-}
+   for (build in job.builds) {
+      def log = build.log
+      if (log.contains('checkout()')) {
+       println "${job.name}: ${build.id}"
+                                }
+                    }
 
+                               }
 }
 }
